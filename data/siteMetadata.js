e5d902bf0e385dfdd7911b1fc4690adc793d0e85
Vatsal <54224422+nebuchad-nezzar@users.noreply.github.com>
2024-10-08 00:52:47 -0400

    updated the /app folder and sitemetadata file.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -11,7 +11,7 @@ const siteMetadata = {
     theme: 'dark', // system, dark or light
     siteUrl: 'https://vatsal.ca',
     siteRepo: 'https://github.com/nebuchad-nezzar',
-    siteLogo: '/data/logo.png',
+    siteLogo: '/static/images/logo.png',
     // image: '/static/images/avatar3.jpeg',
     socialBanner: '/static/images/twittercard.png',
     email: 'vatswork10@gmail.com',
