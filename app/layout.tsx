762a644cbd8f72baace94d9aa762f8c9312bc94b
Vatsal Sharma <vatsal.sharma1997@gmail.com>
2024-11-09 00:06:52 -0500

    Updated Favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             suppressHydrationWarning
         >
             <head>
-                <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+              <title>{siteMetadata.title}</title>
+                <link rel="apple-touch-icon" sizes="76x76" href="static/favicons/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="96x96" href="/static/favicons/favicon-96x96.png" />
+                <link rel="icon" type="image/png" sizes="128*128" href="/static/favicons/favicon-96x96.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
                 <link rel="manifest" href="/static/favicons/site.webmanifest" />
